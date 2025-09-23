613a4342154fe40456da9fb3e7c746d3fcd56c93
Yashnick14 <167408262+Yashnick14@users.noreply.github.com>
2025-09-23 21:47:37 +0530

    Update ReviewRoute.js

--- backend/routes/ReviewRoute.js
+++ backend/routes/ReviewRoute.js
@@ -4,6 +4,7 @@ import {
   createReview,
   deleteReview,
   getReviewsByPlace,
+  getReviewsByUser,
 } from "../controllers/ReviewController.js";
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/", createReview);
 router.get("/place/:placeId", getReviewsByPlace);
 router.delete("/:id", deleteReview);
+router.get("/user/:userId", getReviewsByUser);
 
 export default router;
